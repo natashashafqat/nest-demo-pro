d81891166489d0bd61c875c8e16ea7a651f932e0
Tash Mulla <natashashafqat@gmail.com>
2022-01-27 16:58:30 +0000

    feat(): add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
@@ -8,4 +10,11 @@ import { ProductsModule } from './products/products.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: '/products', method: RequestMethod.POST })
+      .forRoutes(ProductsController);
+  }
+}
